7834ee919d86b3d5bdfec6195d3c2e99d3f34e5b
Sam Nalty <nalty.blade@gmail.com>
2025-11-12 12:04:41 +0000

    improve error handling for wasm work

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,21 @@
+use anyhow::anyhow;
 use noodles::core::Position;
-use serde::{Deserialize, Serialize};
-
-use aws_config::BehaviorVersion;
-use axum::{http::StatusCode, response::Json, routing::get, Router};
-use noodles::core::region::Interval;
 use noodles::csi::binning_index::BinningIndex;
-use std::env;
+use noodles::{core::region::Interval, csi::binning_index::index};
+use serde::{Deserialize, Serialize};
+use std::hash::BuildHasher;
 use std::io::Read;
-use tower_service::Service;
+use tracing::{debug, error};
 use worker::*;
 
+use rapidhash::fast::SeedableState;
+use tracing_subscriber::fmt::format::Pretty;
+use tracing_subscriber::fmt::time::UtcTime;
+use tracing_subscriber::prelude::*;
+use tracing_web::{performance_layer, MakeWebConsoleWriter};
+
 #[derive(Debug, Clone, Deserialize, Serialize)]
-pub struct AlphamissenseResult {
+struct AlphamissenseResult {
     chrom: String,
     pos: i32,
     r#ref: String,
@@ -24,13 +28,14 @@ pub struct AlphamissenseResult {
     am_class: String,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Hash)]
+#[serde(rename_all = "lowercase")]
 enum Genome {
     Hg19,
     Hg38,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Hash)]
 pub struct AlphamissenseQuery {
     chrom: String,
     pos: i32,
@@ -39,92 +44,170 @@ pub struct AlphamissenseQuery {
     genome: Genome,
 }
 
-async fn get_data_from_s3(
-    genome: Genome,
-    chrom: &str,
-    pos: i32,
-    r#ref: &str,
-    alt: &str,
-) -> anyhow::Result<Option<AlphamissenseResult>> {
-    let (index_key, data_key) = match genome {
-        Genome::Hg19 => (
-            env::var("HG19_INDEX_KEY").expect("hg19 index env var should be set"),
-            env::var("HG19_KEY").expect("hg19 env var should be set"),
-        ),
-        Genome::Hg38 => (
-            env::var("HG38_INDEX_KEY").expect("env var should be set"),
-            env::var("HG38_KEY").expect("env var should be set"),
-        ),
+#[event(start)]
+fn start() {
+    // Initialise tracing
+    let fmt_layer = tracing_subscriber::fmt::layer()
+        .json()
+        .with_ansi(false)
+        .with_timer(UtcTime::rfc_3339())
+        .with_writer(MakeWebConsoleWriter::new().with_pretty_level());
+    let perf_layer = performance_layer().with_details_from_fields(Pretty::default());
+    tracing_subscriber::registry()
+        .with(fmt_layer)
+        .with(perf_layer)
+        .init();
+}
+
+async fn get_index_from_r2(index_key: &str, bucket: &Bucket) -> anyhow::Result<Vec<u8>> {
+    let index_output = bucket
+        .get(index_key)
+        .execute()
+        .await?
+        .ok_or(anyhow!(
+            "Failed to get index bytes from KV with key {index_key}"
+        ))
+        .inspect_err(|e| error!("{}", e))?;
+
+    debug!("Got index from R2");
+
+    let body = index_output
+        .body()
+        .ok_or_else(|| anyhow!("Index response didn't have a body"))?;
+
+    let index_bytes = body
+        .bytes()
+        .await
+        .inspect_err(|e| error!("Failed to get body bytes with error: {e}"))?;
+
+    Ok(index_bytes)
+}
+
+/// Get an index from R2 or the KV store
+async fn get_index(
+    indexes_store: &KvStore,
+    index_key: &str,
+    bucket: &Bucket,
+) -> anyhow::Result<noodles::csi::binning_index::Index<Vec<noodles::bgzf::VirtualPosition>>> {
+    let index_output = match indexes_store.get(index_key).bytes().await {
+        Ok(Some(b)) => b,
+        Err(e) => {
+            error!(
+                "Failed to get index bytes from KV store with error: {:?}",
+                e
+            );
+            return Err(worker::Error::KvError(e).into());
+        }
+        Ok(None) => {
+            debug!("Getting index from R2");
+            let index_bytes = get_index_from_r2(index_key, bucket).await?;
+
+            debug!("Storing index in KV for future use");
+
+            let put_op = indexes_store
+                .put_bytes(index_key, &index_bytes)
+                .map_err(worker::Error::KvError)
+                .inspect_err(|e| error!("Failed to put index bytes in KV with error: {}", e))?;
+
+            put_op.execute().await.map_err(worker::Error::KvError)?;
+
+            index_bytes
+        }
     };
 
-    let bucket = env::var("BUCKET").expect("bucket name env var should be set");
+    let mut reader = noodles::tabix::io::Reader::new(&*index_output);
 
-    let aws_config = aws_config::load_defaults(BehaviorVersion::latest()).await;
-    let client = aws_sdk_s3::Client::new(&aws_config);
-
-    let index_output = client
-        .get_object()
-        .bucket(&bucket)
-        .key(index_key)
-        .send()
-        .await?;
-
-    let body = index_output.body.collect().await?.to_vec();
-
-    println!("{}", body.len());
-
-    let mut reader = noodles::tabix::io::Reader::new(&*body);
+    debug!("Reading index with noodles");
 
     let index = reader.read_index()?;
 
-    let interval = match usize::try_from(pos).map(Position::new) {
+    debug!("Index reading complete");
+
+    Ok(index)
+}
+
+async fn get_data_from_s3(
+    query: &AlphamissenseQuery,
+    bucket: &Bucket,
+    env: &Env,
+) -> anyhow::Result<Option<AlphamissenseResult>> {
+    debug!("Starting data retrieval from R2");
+
+    // Get data URIs
+    let (index_key, data_key) = match &query.genome {
+        Genome::Hg19 => (env.var("HG19_INDEX_KEY")?, env.var("HG19_KEY")?),
+        Genome::Hg38 => (env.var("HG38_INDEX_KEY")?, env.var("HG38_KEY")?),
+    };
+
+    let indexes_store = env.kv("INDEXES")?;
+
+    let interval = match usize::try_from(query.pos).map(Position::new) {
         Ok(Some(start)) => Ok(Interval::from(start..=start)),
         _ => Err(anyhow::Error::msg("Failed to create interval")),
     }?;
 
-    let index_header = index.header().unwrap();
+    let index = get_index(&indexes_store, &index_key.to_string(), bucket).await?;
 
-    println!("{:?}", index_header.reference_sequence_names());
+    let index_header = index
+        .header()
+        .ok_or(anyhow::Error::msg("Tabix index did not have a header!"))
+        .inspect_err(|e| error!("{e}"))?;
 
-    let sequence_index = index_header
+    debug!("Getting index of reference sequence in tabix index header");
+    let sequence_index = match index_header
         .reference_sequence_names()
-        .get_index_of(chrom.as_bytes())
-        .unwrap();
+        .get_index_of(query.chrom.as_bytes())
+    {
+        Some(i) => i,
+        None => return Ok(None),
+    };
 
+    debug!("Getting blocks that contain ROI");
     let blocks_to_fetch = index.query(sequence_index, interval)?;
 
+    debug!("Iterating over chunks returned");
     // now let's get the blocks
     for chunk in blocks_to_fetch {
+        debug!("In chunk iteration");
         let start = chunk.start();
         let end = chunk.end();
+        let len = end.compressed() - start.compressed();
 
-        println!(
-            "Chunk: compressed {}..{}, uncompressed {}..{}",
-            start.compressed(),
-            end.compressed(),
-            start.uncompressed(),
-            end.uncompressed()
-        );
+        debug!("Getting required range of bytes from scores file using ranged request");
+        let chunk_object_get_op = bucket
+            .get(data_key.to_string())
+            .range(Range::OffsetWithLength {
+                offset: start.compressed(),
+                length: len,
+            });
 
-        let data_output = client
-            .get_object()
-            .bucket(&bucket)
-            .key(&data_key)
-            .range(format!("bytes={}-{}", start.compressed(), end.compressed()))
-            .send()
+        let chunk_object = chunk_object_get_op
+            .execute()
             .await?
-            .body
-            .collect()
-            .await
-            .unwrap()
-            .to_vec();
+            .ok_or(anyhow::Error::msg(
+                "Ranged request didn't get an Object from R2",
+            ))?;
 
-        let mut bgzf_reader = noodles::bgzf::io::Reader::new(std::io::Cursor::new(data_output));
+        let chunk_body = chunk_object
+            .body()
+            .ok_or(anyhow::Error::msg("R2 object didn't have a body"))
+            .inspect_err(|e| error!("{e}"))?;
+
+        let chunk_bytes = chunk_body
+            .bytes()
+            .await
+            .inspect_err(|e| error!("Failed to get bytes from R2 object with error: {e}"))?;
+
+        debug!("Got the required byte ranges");
+
+        let mut bgzf_reader = noodles::bgzf::io::Reader::new(&*chunk_bytes);
 
         let mut buf = Vec::new();
 
+        debug!("Reading bgzipped data");
         bgzf_reader.read_to_end(&mut buf)?;
 
+        // Trim leading bytes to start position in bgzf block.
         let buf = &buf[start.uncompressed() as usize..];
 
         let mut csv_reader = csv::ReaderBuilder::new()
@@ -133,14 +216,17 @@ async fn get_data_from_s3(
             .flexible(true)
             .from_reader(buf);
 
+        debug!("Iterating over uncompressed records until we find the requested record");
         for record in csv_reader.deserialize() {
             let record: AlphamissenseResult = record?;
+            debug!("Record: {:?}", record);
 
-            if record.chrom == chrom
-                && record.pos == pos
-                && record.r#ref == r#ref
-                && record.alt == alt
+            if record.chrom == query.chrom
+                && record.pos == query.pos
+                && record.r#ref == query.ref_allele
+                && record.alt == query.alt_allele
             {
+                debug!("Got a hit for this request");
                 return Ok(Some(record));
             }
         }
@@ -148,49 +234,81 @@ async fn get_data_from_s3(
     Ok(None)
 }
 
-#[axum::debug_handler]
-pub async fn get_variant(
-    Json(query): Json<AlphamissenseQuery>,
-) -> axum::response::Result<Json<Option<AlphamissenseResult>>> {
-    let result = get_data_from_s3(
-        query.genome,
-        &query.chrom,
-        query.pos,
-        &query.ref_allele,
-        &query.alt_allele,
-    )
-    .await;
+async fn get_result_from_cache_or_s3(
+    query: &AlphamissenseQuery,
+    cache_store: &KvStore,
+    bucket: &Bucket,
+    env: &Env,
+) -> anyhow::Result<Option<AlphamissenseResult>> {
+    // Create hasher to hash our query to check KV for result
+    let query_hash = SeedableState::fixed().hash_one(&query).to_string();
 
-    match result {
-        Ok(Some(r)) => Ok(Json(Some(r))),
-        _ => Err(StatusCode::INTERNAL_SERVER_ERROR.into()),
+    let cache_response = cache_store
+        .get(&query_hash)
+        .json::<AlphamissenseResult>()
+        .await
+        .map_err(worker::Error::KvError)
+        .inspect_err(|e| error!("Failed to get result from cache with error: {e}"))?;
+
+    if let Some(response) = cache_response {
+        Ok(Some(response))
+    } else {
+        let result = get_data_from_s3(query, bucket, env).await;
+
+        if let Ok(Some(r)) = &result {
+            let result_put_op = cache_store
+                .put(&query_hash, r)
+                .map_err(worker::Error::KvError)
+                .inspect_err(|e| error!("Failed to build KV put request with error: {e}"))?;
+
+            result_put_op
+                .execute()
+                .await
+                .map_err(worker::Error::KvError)
+                .inspect_err(|e| error!("Failed to execute KV put request with error: {e}"))?;
+        };
+
+        result
     }
 }
 
-fn router() -> Router {
-    Router::new().route("/", get(get_variant))
+async fn get_variant(request: Request, ctx: RouteContext<()>) -> worker::Result<Response> {
+    let query: AlphamissenseQuery = request
+        .query()
+        .inspect_err(|e| error!("Failed to deserialize query with error: {e}"))?;
+
+    // Check to see if it's cached
+    let cache_store = ctx
+        .env
+        .kv("CACHE")
+        .inspect_err(|e| error!("Failed to get cache KV store with error: {e}"))?;
+
+    // Get bucket
+    let bucket = ctx
+        .bucket("BUCKET")
+        .inspect_err(|e| error!("Failed to get bucket from context with error: {e}"))?;
+
+    let result = get_result_from_cache_or_s3(&query, &cache_store, &bucket, &ctx.env).await;
+
+    let response = match result {
+        Ok(Some(r)) => Response::from_json(&r),
+        Ok(None) => Response::from_json(&()),
+        Err(e) => {
+            error!("Failed to get alphamissense result with error: {e}");
+            Err(worker::Error::Internal("500".into()))
+        }
+    };
+
+    // Add CORS headers to response
+    let cors = Cors::new().with_origins(["*"]);
+
+    response.map(|r| r.with_cors(&cors))?
 }
 
 #[event(fetch)]
-async fn fetch(
-    req: HttpRequest,
-    _env: Env,
-    _ctx: Context,
-) -> Result<http::Response<axum::body::Body>> {
-    Ok(router().call(req).await?)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[tokio::test]
-    async fn test_get() {
-        dotenvy::dotenv().unwrap();
-        let result = get_data_from_s3(Genome::Hg19, "chr1", 55523855, "G", "A")
-            .await
-            .unwrap();
-
-        println!("{result:?}");
-    }
+async fn fetch(req: Request, env: Env, _ctx: Context) -> worker::Result<Response> {
+    Router::new()
+        .get_async("/v1/variant", get_variant)
+        .run(req, env)
+        .await
 }
